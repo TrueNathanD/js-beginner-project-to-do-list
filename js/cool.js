4da14adeca1cb4a843b871dceda5c02996fee67c
Nathan D <truenathand@gmail.com>
2022-04-11 17:56:55 -0500

    Added delete feature, cleaned up UI color, size, and spacing

--- js/cool.js
+++ js/cool.js
@@ -8,6 +8,8 @@ const todoList = document.querySelector('.todo-list');
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
 
+todoList.addEventListener('click', deleteCheck);
+
 // Functions
 function addTodo(event) {
     // Prevents form from submitting
@@ -17,7 +19,7 @@ function addTodo(event) {
     todoDiv.classList.add('todo');
     // Creates LI
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     // Check Mark button
@@ -28,8 +30,19 @@ function addTodo(event) {
     // Check Trash button
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add('complete-btn');
+    trashButton.classList.add('trash-btn');
     todoDiv.appendChild(trashButton);
     // Append to list
     todoList.appendChild(todoDiv);
+    // Clear Todo Input value
+    todoInput.value = '';
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    // Delete Todo
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        todo.remove();
+    }
 }
